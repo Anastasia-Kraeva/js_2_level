d6f50dca5d5b4ba3d633f883d9f219bee9a39e06
Kraeva Anastasia <kraeva811625@gmail.com>
2021-01-26 20:54:32 +0300

    lesson-3

--- script_2.js
+++ script_2.js
@@ -1,14 +1,28 @@
-const app = new Vue({
-    el: '#app',
+Vue.component('goods-list', {
+    props: ['goods'],
+    template: `
+      <div class="list goods-list">
+        <goods-item v-for="good in goods" v-bind:good="good" v-bind:key="good.id_product"></goods-item>
+      </div>
+    `
+});
+
+Vue.component('goods-item', {
+    props: ['good'],
+    template: `
+      <div class="item goods-item">
+        <h3>{{ good.product_name }}</h3>
+        <p>{{ good.price }}</p>
+      </div>
+    `
+});
+
+var catalogApp = new Vue({
+    el: '#catalog',
     data: {
-        goods: [],
         filteredGoods: [],
-        searchLine: '',
         API_URL: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
-        searchLine: '',
         isVisibleCatalog: true,
-        cart: [],
-        isVisibleCart: false,
     },
     methods: {
         makeGETRequest(url, callback) {
@@ -31,41 +45,135 @@ const app = new Vue({
         },
 
         mounted() {
-            this.makeGETRequest(`${app.API_URL}/catalogData.json`, (goods) => {
-                this.goods = JSON.parse(goods);
+            this.makeGETRequest(`${this.API_URL}/catalogData.json`, (goods) => {
+                this.filteredGoods = JSON.parse(goods);
             });
         },
-
     },
-});
-app.mounted()
+    beforeMount() {
+        this.mounted()
+    }
 
-const formSearch = new Vue({
-    el: '#search_form',
-    data: {
-        searchLine: null,
+});
+
+Vue.component('search-form', {
+    template: `
+        <div>
+            <form id="search_form" @submit.prevent="FilterGoods">
+                <input class="goods_search" id="search_line" v-model='searchLine'>
+                <input type="submit" value="Искать">
+            </form>
+        </div>
+    `,
+    data() {
+        return {
+            searchLine: null,
+        }
     },
     methods: {
-        FilterGoods: function (e) {
-            let obj = app.goods;
-            const search = this.searchLine.toLowerCase();
+        FilterGoods() {
+            let obj = catalogApp.filteredGoods;
+            let search = this.searchLine.toLowerCase();
+            catalogApp.filteredGoods = []
             for (key in obj) {
                 el = obj[key]
-                if (el.product_name.toLowerCase().indexOf(this.searchLine) != -1) app.filteredGoods.push(el)
+
+                if (el.product_name.toLowerCase().indexOf(search) != -1) catalogApp.filteredGoods.push(el)
             }
-        }
+        },
     }
 });
 
-const cart = new Vue({
-    el: '.cart-button',
+var formSearch = new Vue({
+    el: '#nav',
     data: {
-        cart: [],
+        filteredGoods: [],
     },
     methods: {
-        VisibleCart: function () {
-            app.isVisibleCatalog = false;
-            app.isVisibleCart = true;
-        }
+        VisibleCart() {
+            catalogApp.isVisibleCatalog = false;
+            cartApp.isVisibleCart = true;
+        },
     }
 });
+
+Vue.component('cart-list', {
+    props: ['cartList'],
+    template: `
+    <div class="list cart-list">
+        <cart-item v-for="item in cartList" v-bind:item="item" v-bind:key="item.id_product"></cart-item>
+    </div>
+    `
+});
+
+Vue.component('cart-item', {
+    props: ['item'],
+    template: `
+        <div class="item cart-item">
+            <h3 class="title">{{ item.product_name }}</h3>
+            <p class="price">{{ item.price }}</p>
+        </div>
+    `
+});
+
+const cartApp = new Vue({
+    el: '#cart',
+    data: {
+        cartArr: [{ product_name: 'Мышка', price: 1000, id_product: '456' }],
+        isVisibleCart: false,
+    },
+    methods: {
+
+    }
+});
+// Vue.component('error', {
+//     props: ['error'],
+//     template: `
+//     <div v-if=''>
+//         Error: не удаётся выполнить запрос к серверу
+//     </div>
+//     `
+// });
+
+// const app = new Vue({
+//     el: '#app',
+
+// data: {
+//     goods: [],
+//     filteredGoods: [],
+//     searchLine: '',
+//     API_URL: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
+//     searchLine: '',
+//     isVisibleCatalog: true,
+//     cart: [],
+//     isVisibleCart: false,
+// },
+// methods: {
+//     makeGETRequest(url, callback) {
+//         var xhr;
+
+//         if (window.XMLHttpRequest) {
+//             xhr = new XMLHttpRequest();
+//         } else if (window.ActiveXObject) {
+//             xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//         }
+
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState === 4) {
+//                 callback(xhr.responseText);
+//             }
+//         }
+
+//         xhr.open('GET', url, true);
+//         xhr.send();
+//     },
+
+//     mounted() {
+//         this.makeGETRequest(`${app.API_URL}/catalogData.json`, (goods) => {
+//             this.goods = JSON.parse(goods);
+//         });
+//     },
+
+// },
+// });
+// catalogApp.mounted()
